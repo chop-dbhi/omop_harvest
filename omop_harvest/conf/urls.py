b93caf453d31578364a421ab9aeec5f99edcfe49
Le Mar Davidson <davidsonl2@email.chop.edu>
2014-07-02 14:35:36 -0400

    Adding urls.py specific to omop harvest

--- omop_harvest/conf/urls.py
+++ omop_harvest/conf/urls.py
@@ -21,13 +21,30 @@ urlpatterns = patterns(
     url(r'^api/', include('serrano.urls')),
     # Administrative components
     url(r'^admin/', include(admin.site.urls)),
+
+    #CHOPAuth URLs
+    url(r'^register/$', 'registration.views.register',{'template_name':'registration.html'},name='register'),
+    url(r'^register/complete/$', TemplateView.as_view(template_name='registration_complete.html'),
+        name='registration-complete'),
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name':'login.html'}, name='login'),
 )
 
+# If chopauth is available, include those urls
+try:
+    urlpatterns += patterns('',
+        url(r'^', include('chopauth.urls')),
+    )
+except ImportError:
+    urlpatterns += patterns('',
+        url(r'^', include('registration.urls')),
+    )
+
 # In production, these two locations must be served up statically
-urlpatterns += patterns(
-    'django.views.static',
-    url(r'^{0}(?P<path>.*)$'.format(
-        re.escape(settings.MEDIA_URL.lstrip('/'))), 'serve', {'document_root': settings.MEDIA_ROOT}
-        ),
-    url(r'^{0}(?P<path>.*)$'.format(re.escape(settings.STATIC_URL.lstrip('/'))), 'serve', {'document_root': settings.STATIC_ROOT}),
+urlpatterns += patterns('django.views.static',
+    url(r'^{0}(?P<path>.*)$'.format(re.escape(settings.MEDIA_URL.lstrip('/'))), 'serve', {
+        'document_root': settings.MEDIA_ROOT
+    }),
+    url(r'^{0}(?P<path>.*)$'.format(re.escape(settings.STATIC_URL.lstrip('/'))), 'serve', {
+        'document_root': settings.STATIC_ROOT
+    }),
 )
