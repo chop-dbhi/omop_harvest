065826cfd69901f7b2b39fe766cda9016fa47079
Aaron Browne <aaron0browne@gmail.com>
2014-07-23 15:39:29 -0400

    Fix OSIM data load script, start using OSIM SNOMED
    The OSIM2_data_load.psql script had a word order error that caused it to
    fail, this is fixed. That script as well as the pre-processing scripts (remove
    pipes and quotes and remove CDMV2 fields scripts) have been changed to
    target the OSIM2_1M_MSLR_SNOMED_0_CSV dataset because it is much more
    manageable than the 10M dataset used previously.
    
    Signed-off-by: Aaron Browne <aaron0browne@gmail.com>

--- etl/remove_CDMV2_fields.sh
+++ etl/remove_CDMV2_fields.sh
@@ -1,6 +1,6 @@
-awk 'BEGIN{FS=",";OFS=","}{print $1,$2,$3,$5,$6,$7,$8}' OSIM2_10M_MSLR_MEDDRA_11_CSV/condition_era.csv > OSIM2_10M_MSLR_MEDDRA_11_CSV/new_condition_era.csv
-mv OSIM2_10M_MSLR_MEDDRA_11_CSV/new_condition_era.csv OSIM2_10M_MSLR_MEDDRA_11_CSV/condition_era.csv
-awk 'BEGIN{FS=",";OFS=","}{print $1,$2,$3,$4}' OSIM2_10M_MSLR_MEDDRA_11_CSV/observation_period.csv > OSIM2_10M_MSLR_MEDDRA_11_CSV/new_observation_period.csv
-mv OSIM2_10M_MSLR_MEDDRA_11_CSV/new_observation_period.csv OSIM2_10M_MSLR_MEDDRA_11_CSV/observation_period.csv
-awk 'BEGIN{FS=",";OFS=","}{print $1,$2,$3,$4,$6,$7,$9}' OSIM2_10M_MSLR_MEDDRA_11_CSV/person.csv > OSIM2_10M_MSLR_MEDDRA_11_CSV/new_person.csv
-mv OSIM2_10M_MSLR_MEDDRA_11_CSV/new_person.csv OSIM2_10M_MSLR_MEDDRA_11_CSV/person.csv
+awk 'BEGIN{FS=",";OFS=","}{print $1,$2,$3,$5,$6,$7,$8}' OSIM2_1M_MSLR_SNOMED_0_CSV/condition_era.csv > OSIM2_1M_MSLR_SNOMED_0_CSV/new_condition_era.csv
+mv OSIM2_1M_MSLR_SNOMED_0_CSV/new_condition_era.csv OSIM2_1M_MSLR_SNOMED_0_CSV/condition_era.csv
+awk 'BEGIN{FS=",";OFS=","}{print $1,$2,$3,$4}' OSIM2_1M_MSLR_SNOMED_0_CSV/observation_period.csv > OSIM2_1M_MSLR_SNOMED_0_CSV/new_observation_period.csv
+mv OSIM2_1M_MSLR_SNOMED_0_CSV/new_observation_period.csv OSIM2_1M_MSLR_SNOMED_0_CSV/observation_period.csv
+awk 'BEGIN{FS=",";OFS=","}{print $1,$2,$3,$4,$6,$7,$9}' OSIM2_1M_MSLR_SNOMED_0_CSV/person.csv > OSIM2_1M_MSLR_SNOMED_0_CSV/new_person.csv
+mv OSIM2_1M_MSLR_SNOMED_0_CSV/new_person.csv OSIM2_1M_MSLR_SNOMED_0_CSV/person.csv
