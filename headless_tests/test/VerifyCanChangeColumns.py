a2ad78a1db7d2875191f0e1c6bd32633094873be
Sheik Hassan <solergiga@yahoo.com>
2014-07-31 14:15:48 -0400

    Several Headless Tests
    Signed-off-by: Sheik Hassan <solergiga@yahoo.com>

--- headless_tests/test/VerifyCanChangeColumns.py
+++ headless_tests/test/VerifyCanChangeColumns.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 import unittest, time, re
 
-class VerifyHeadersExist(unittest.TestCase):
+class VerifyCanChangeColumns(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.PhantomJS()
         self.driver.implicitly_wait(30)
@@ -13,7 +13,7 @@ class VerifyHeadersExist(unittest.TestCase):
         self.verificationErrors = []
         self.accept_next_alert = True
     
-    def test_verify_headers_exist(self):
+    def test_verify_can_change_columns(self):
         driver = self.driver
         driver.get(self.base_url + "/login/")
         driver.find_element_by_id("id_username").clear()
@@ -21,11 +21,29 @@ class VerifyHeadersExist(unittest.TestCase):
         driver.find_element_by_id("id_password").clear()
         driver.find_element_by_id("id_password").send_keys("test")
         driver.find_element_by_css_selector("button.btn-info.btn").click()
-        driver.find_element_by_link_text("Workspace").click()
-        driver.find_element_by_xpath("//div[@id='wrap']/div/div/div/ul/li[2]/a/span").click()
-        driver.find_element_by_xpath("//div[@id='wrap']/div/div/div/ul/li[3]/a/span").click()
-        driver.find_element_by_link_text("Query").click()
-        driver.find_element_by_link_text("test").click()
+        for i in range(60):
+            try:
+                if self.is_element_present(By.CSS_SELECTOR, "div.info-region"): break
+            except: pass
+            time.sleep(1)
+        else: self.fail("time out")
+        driver.find_element_by_xpath("//button[2]").click()
+        driver.find_element_by_xpath("//div[@id='content']/div[8]/div/div/div/button").click()
+        driver.find_element_by_xpath("//div[@id='content']/div[4]/div[2]/div[2]/div/div/div[2]/div/div/div/button").click()
+        driver.find_element_by_xpath("//div[@id='content']/div[4]/div[3]/button[2]").click()
+        driver.find_element_by_xpath("//div[@id='content']/div[8]/div/div/div/button").click()
+        driver.find_element_by_xpath("//button[@type='button']").click()
+        driver.find_element_by_css_selector("li > button.btn.btn-mini").click()
+        driver.find_element_by_xpath("//div[@id='content']/div[4]/div[2]/div[2]/div/div/div[2]/div/div/div[2]/div/div/ul/li[2]/button").click()
+        driver.find_element_by_xpath("//div[@id='content']/div[4]/div[2]/div[2]/div/div/div[2]/div/div/div[2]/div/div/ul/li[3]/button").click()
+        driver.find_element_by_xpath("//div[@id='content']/div[4]/div[3]/button[2]").click()
+        for i in range(60):
+            try:
+                if self.is_element_present(By.CSS_SELECTOR, "tbody"): break
+            except: pass
+            time.sleep(1)
+        else: self.fail("time out")
+        driver.find_element_by_link_text("user002").click()
         driver.find_element_by_link_text("Logout").click()
     
     def is_element_present(self, how, what):
