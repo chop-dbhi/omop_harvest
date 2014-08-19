90612828c5735680622b6e792517e02fbe6a25fb
Aaron Browne <aaron0browne@gmail.com>
2014-08-19 15:06:04 -0400

    Debugging syncdb
    Signed-off-by: Aaron Browne <aaron0browne@gmail.com>

--- cid/my_init.d/01_application_init.sh
+++ cid/my_init.d/01_application_init.sh
@@ -5,6 +5,8 @@ cd $APP_DIR
 
 source /opt/ve/bin/activate
 
+echo "APP_ENV: " $APP_ENV
+
 python bin/manage.py syncdb --noinput
 python bin/manage.py migrate --noinput
 python bin/manage.py collectstatic --noinput
